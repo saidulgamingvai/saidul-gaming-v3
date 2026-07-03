6fb94525dd69c5ef8eaa6ddda76f2c94a3a2c16c
saidulgamingvai <educationghore@gmail.com>
2026-07-03 18:20:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
 
+// ==============================
+// Saidul Gaming Vai Live Engine
+// ==============================
+
+let match = JSON.parse(localStorage.getItem("match")) || CONFIG;
+
+// Save Data
+function saveMatch() {
+    localStorage.setItem("match", JSON.stringify(match));
+}
+
+// Update Screen
+function updateScreen() {
+
+    document.getElementById("homeTeam").innerText =
+        match.homeTeam.name;
+
+    document.getElementById("awayTeam").innerText =
+        match.awayTeam.name;
+
+    document.getElementById("homeLogo").src =
+        match.homeTeam.logo;
+
+    document.getElementById("awayLogo").src =
+        match.awayTeam.logo;
+
+    document.getElementById("score").innerText =
+        `${match.homeTeam.score} - ${match.awayTeam.score}`;
+
+    document.getElementById("timer").innerText =
+        String(match.timer.minute).padStart(2,"0") + ":" +
+        String(match.timer.second).padStart(2,"0");
+
+    document.getElementById("commentary").innerText =
+        match.commentary;
+
+}
+
+// Timer
+setInterval(() => {
+
+    if(match.timer.running){
+
+        match.timer.second++;
+
+        if(match.timer.second >= 60){
+
+            match.timer.second = 0;
+            match.timer.minute++;
+
+        }
+
+        saveMatch();
+        updateScreen();
+
+    }
+
+},1000);
+
+// First Load
+updateScreen();
